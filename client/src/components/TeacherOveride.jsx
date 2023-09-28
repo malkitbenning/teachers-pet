76be4d0a0d1f18526b481872711fc86a51b184c0
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-09-28 19:30:23 +0100

    Updated override comment
    Updated css class name and seperated into two rows

--- client/src/components/TeacherOveride.jsx
+++ client/src/components/TeacherOveride.jsx
@@ -1,15 +1,24 @@
+import "../styles/TeacherOverride.css";
+
 function TeacherOverride({ totalScore, setTotalScore }) {
   return (
-    <tr>
-      <td colSpan="3" className="comment-section">
-        <span className="comments">Comments</span>
-        <textarea></textarea>
-      </td>
-      <td>CYP Score</td>
-      <td>
-        <input value={totalScore} onChange={(e) => setTotalScore(Number(e.target.value))} />
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td>CYP Total</td>
+        <td>
+          <input
+            value={totalScore}
+            onChange={(e) => setTotalScore(Number(e.target.value))}
+          />
+        </td>
+      </tr>
+      <tr>
+        <td colSpan="3" className="override-comment">
+          <span className="comments">Override Comment</span>
+          <textarea></textarea>
+        </td>
+      </tr>
+    </>
   );
 }
 export default TeacherOverride;
