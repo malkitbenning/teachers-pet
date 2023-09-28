cd671ecd9c4dbe043211df1c82548d9b3e542cc5
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-09-28 20:02:01 +0100

    Renamed TeacherOverride component

--- client/src/components/ShowResult.jsx
+++ client/src/components/ShowResult.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import TeacherOverride from "./TeacherOveride";
+import TeacherOverride from "./TeacherOverride";
 
 function ShowResult({
   selectedAnswers,
