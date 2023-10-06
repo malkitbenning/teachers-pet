9e8dfd384ecb9ca8a4e550f0350fbfe14131c3e6
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-10-06 21:33:03 +0100

    Deleted selected_choices and form_qa tables

--- db-structure/create_selected_choices.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE selected_choices (
-    choices_id           SERIAL PRIMARY KEY,
-    pupil_id             INTEGER NOT NULL REFERENCES pupil(pupil_id),
-    selected_qa_id       INTEGER NOT NULL REFERENCES selected_qa(selected_qa_id)
-    );
--- db-structure/form_qa.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE form_qa (
-  form_qa_id           SERIAL PRIMARY KEY,
-  question_id          INTEGER NOT NULL REFERENCES question (question_id),
-  answer_id            INTEGER NOT NULL REFERENCES answer (answer_id)
-);
