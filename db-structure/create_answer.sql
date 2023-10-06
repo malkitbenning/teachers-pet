c5fee17738e51386b776c6e56fc8f668d0570300
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-10-06 21:52:48 +0100

    Amended answer table
    Added question_id as a foreign key
    
    Edited answer_text to VARCHAR(500)

--- db-structure/create_answer.sql
+++ db-structure/create_answer.sql
@@ -1,6 +1,7 @@
 CREATE TABLE answer (
   answer_id          SERIAL PRIMARY KEY,
+  question_id        INTEGER NOT NULL REFERENCES question(question_id),
   option_position    INTEGER NOT NULL,
-  answer_text        VARCHAR(255) NOT NULL,
+  answer_text        VARCHAR(500) NOT NULL,
   answer_score       INTEGER NOT NULL
 );
