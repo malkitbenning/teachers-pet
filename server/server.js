9c3251f91c24dbed0a9865b9f657888e51647a27
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-10-06 13:29:32 +0100

    Updated login endpoint
    Changed app.post to app.get. The method call for the endpoint is a GET as we're retrieving and validating login data. We're not posting a record to be inserted to a database.
    
    Rearranged function and destructured props based on tech lead feedback.
    
    Removed test code.

--- server/server.js
+++ server/server.js
@@ -13,45 +13,16 @@ app.listen(expressPort, () =>
   console.log(`Listening on expressPort ${expressPort}`)
 );
 
-app.get("/", function (req, res) {
-  const testScore = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    pupilName: "Thomas",
-    pupilScore: testScore,
-  });
-});
-
-app.post("/pupilRecord", (req, res) => {
-  if (!req.body.pupilName || !req.body.pupilScore) {
-    res.status(400).json({
-      result: "failure",
-      message: "Pupil details could not be saved",
-    });
-  }
-  let reqPupilName = req.body.pupilName;
-  console.log("pupil name", reqPupilName);
-  res.status(201).json({
-    pupilName: reqPupilName,
-  });
-});
-
-// endpoint to display login page
-app.get("/login", (req, res) => {
-  console.log("Hey there");
-  res.status(200).send("info sent to user");
-});
-
-// endpoint to validate user credentials
-app.post("/login", (req, res) => {
-  console.log(req.body);
-
-  const tempUsername = req.body.teacherUsername;
-  const tempPassword = req.body.teacherPassword;
+const validateUser = (req, res) => {
+  const { teacherUsername, teacherPassword } = req.body;
 
   // check if matches specific string
-  if (tempUsername === "benning123" && tempPassword === "abc123") {
+  if (teacherUsername === "benning123" && teacherPassword === "abc123") {
     res.status(200).json({ message: "login details correct", teacherID: "3" });
   } else {
     res.status(401).json({ error: "incorrect login details" });
   }
-});
+};
+
+// endpoint to validate user credentials
+app.get("/login", validateUser);
