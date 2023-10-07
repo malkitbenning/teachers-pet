68df6fff7889e0566a98028278e0e63365275aff
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-10-07 15:13:43 +0100

    removed question_id from selection_option table

--- db-structure/create_selected_option.sql
+++ db-structure/create_selected_option.sql
@@ -1,17 +1,13 @@
 CREATE TABLE selected_option (
     pupil_id                   INTEGER NOT NULL,
-    question_id                INTEGER NOT NULL,
     answer_id                  INTEGER NOT NULL,
     teacher_comment            VARCHAR(255),
     PRIMARY KEY (pupil_id, question_id, answer_id),
     CONSTRAINT fk_pupil 
         FOREIGN KEY(pupil_id) 
             REFERENCES pupil(pupil_id),
-    CONSTRAINT fk_question 
-        FOREIGN KEY(question_id) 
-            REFERENCES question(question_id),
     CONSTRAINT fk_answer
         FOREIGN KEY(answer_id) 
-            REFERENCES answer(answer_id)      
+            REFERENCES answer(answer_id)     
 );
     
