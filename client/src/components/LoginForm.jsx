49cac364dfb12ceab54feb39eaa3801ac5ba20f2
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-10-08 22:21:55 +0100

    Added useState for username and password
    Added an onChange event handler to username and password inputs

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import LoginButton from "./LoginButton";
 
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   function handleValidateUser(event) {
     event.preventDefault();
     console.log("validating user");
@@ -15,12 +18,30 @@ function LoginForm() {
           <div className="text">Login </div>
           <form className="loginForm" action="#" onSubmit={handleValidateUser}>
             <div className="field">
-              <input type="text" required id="username" name="username" />
+              <input
+                type="text"
+                required
+                id="username"
+                name="username"
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                  console.log(event.target.value);
+                }}
+              />
               <span className="fas fa-user"></span>
               <label htmlFor="username">User Name </label>
             </div>
             <div className="field">
-              <input type="password" required id="password" name="password" />
+              <input
+                type="password"
+                required
+                id="password"
+                name="password"
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                  console.log(event.target.value);
+                }}
+              />
               <span className="fas fa-lock"></span>
               <label htmlFor="password">Password</label>
             </div>
