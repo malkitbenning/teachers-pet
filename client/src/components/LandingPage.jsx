0d6d771cfd3f7cdd60830c156229a568b7f1f38c
fowobi <109865505+fowobi@users.noreply.github.com>
2023-10-09 22:59:26 +0100

    create Summary landingpage table
    I created both frontend and backend for Summary landing page table

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,78 +1,74 @@
 import { Link } from "react-router-dom";
 import "../styles/LandingPage.css";
+import React, { useState, useEffect } from "react";
+
+
 
 function LandingPage() {
+  const [pupils, setPupils] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/PupilWithTotalScoreAndSupportAllocation")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPupils(data); // Assuming data is an array of pupils
+        // setTeacherName(data.teacherName); // Assuming data contains teacher name
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Welcome, Diljit</h1>
-      <Link to="/form">
-        <h2 className="new-form-title">New Form</h2>
-      </Link>
-
-      <table className="table">
-        <tbody>
-          <tr>
-            <th>Pupil Name</th>
-            <th>Last Updated</th>
-
-            <th>Override Score</th>
-            <th>Final Allocation</th>
-            <th>Delete</th>
-          </tr>
-          <tr>
-            <td>Jack</td>
-            <td>06/10/2023</td>
-
-            <td></td>
-            <td>A1 - 2 or more visits per week</td>
-            <td>
-              <button>x</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Jill</td>
-            <td>03/09/2023</td>
-
-            <td></td>
-            <td>C3 - Annual check or visit</td>
-            <td>
-              <button>x</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Millhouse</td>
-            <td>11/09/2023</td>
-
-            <td>26</td>
-            <td>B2 - Twice termly (3 term year)</td>
-            <td>
-              <button>x</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Lisa</td>
-            <td>08/08/2023</td>
-
-            <td></td>
-            <td>NFA - Off caseload</td>
-            <td>
-              <button>x</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Bart</td>
-            <td>08/08/2023</td>
-
-            <td>68</td>
-            <td>A2 - Weekly</td>
-            <td>
-              <button>x</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <h1>Welcome to landing page</h1>
+      <Link to="/form">Add New Pupil</Link>
+      {pupils.length === 0 ? (
+        <p>No pupils data available.</p>
+      ) : (
+        <table className="pupil-table">
+          <thead>
+            <tr>
+              <th>Pupil ID</th>
+              <th>Pupil Nickname</th>
+              <th>Last Update</th>
+              <th>Total Score</th>
+              <th>Override Score</th>
+              <th>Final Support Allocation</th>
+              <th>Summary</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pupils.map((pupil) => (
+              <tr key={pupil.pupil_id}>
+                <td>{pupil.pupil_id}</td>
+                <td>{pupil.pupil_nickname}</td>
+                <td>{pupil.last_update}</td>
+                <td>{pupil.total_score}</td>
+                <td>{pupil.override_score}</td>
+                <td>{pupil.final_support_allocation}</td>
+                <td>
+                  <button>Summary</button>
+                </td>
+                <td>
+                  <button>Edit</button>
+                </td>
+               
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
 
 export default LandingPage;
+
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+const { Client } = require("pg");
 const app = express();
 
 let cors = require("cors");
@@ -9,9 +11,16 @@ app.use(express.urlencoded({ extended: false }));
 
 const expressPort = process.env.PORT || 5000;
 
-app.listen(expressPort, () =>
-  console.log(`Listening on expressPort ${expressPort}`)
-);
+
+const client = new Client({
+  user: process.env.DBUSER,
+  host: process.env.DBHOST,
+  database: process.env.DBDATABASE,
+  password: process.env.DBPASSWORD,
+  port: process.env.DBPORT,
+  ssl: true,
+});
+client.connect();
 
 const validateUser = (req, res) => {
   const { teacherUsername, teacherPassword } = req.body;
@@ -26,3 +35,115 @@ const validateUser = (req, res) => {
 
 // endpoint to validate user credentials
 app.get("/login", validateUser);
+
+app.get("/PupilWithTotalScoreAndSupportAllocation", async (req, res) => {
+  try {
+    // Query 1: Fetch pupil data
+    const pupilDataQueryResult = await client.query(`
+      SELECT
+        p.pupil_id,
+        p.pupil_nickname,
+        p.last_update,
+        p.override_score,
+        p.override_comment
+      FROM
+        pupil p
+    `);
+
+    // Query 2: Calculate total score
+    const totalScoreQueryResult = await client.query(`
+      SELECT
+        p.pupil_id,
+        p.pupil_nickname,
+        p.last_update,
+        p.override_score,
+        COALESCE(SUM(a.answer_score), 0) AS total_score
+      FROM
+        pupil p
+      LEFT JOIN
+        selected_option AS so
+      ON
+        p.pupil_id = so.pupil_id
+      LEFT JOIN
+        answer AS a
+      ON
+        so.answer_id = a.answer_id
+      GROUP BY
+        p.pupil_id, p.pupil_nickname, p.last_update, p.override_score;
+    `);
+
+    // Query 3: Calculate final support allocation
+    const finalSupportAllocationQueryResult = await client.query(`
+      WITH total_scores AS (
+        SELECT
+          p.pupil_id,
+          p.pupil_nickname,
+          p.last_update,
+          p.override_score,
+          COALESCE(SUM(a.answer_score), 0) AS total_score
+        FROM
+          pupil p
+        LEFT JOIN
+          selected_option AS so
+        ON
+          p.pupil_id = so.pupil_id
+        LEFT JOIN
+          answer AS a
+        ON
+          so.answer_id = a.answer_id
+        GROUP BY
+          p.pupil_id, p.pupil_nickname, p.last_update, p.override_score
+      ),
+      support_allocation_override AS (
+        SELECT
+          ts.pupil_id,
+          ts.pupil_nickname,
+          ts.last_update,
+          ts.override_score,
+          CONCAT(
+            matrix.support_category,
+            ' | ',
+            matrix.support_allocation_text
+          ) AS final_support_allocation
+        FROM
+          total_scores AS ts
+        INNER JOIN
+          support_allocation_matrix AS matrix
+        ON
+          ts.override_score >= matrix.range_minimum
+          AND ts.override_score <= matrix.range_maximum
+      )
+      SELECT * FROM support_allocation_override;
+    `);
+
+    // Combine the results from the queries into a single array of objects
+    const combinedData = pupilDataQueryResult.rows.map((pupilData) => {
+      const totalScoreData = totalScoreQueryResult.rows.find(
+        (scoreData) => scoreData.pupil_id === pupilData.pupil_id
+      );
+      const finalSupportAllocationData =
+        finalSupportAllocationQueryResult.rows.find(
+          (allocationData) => allocationData.pupil_id === pupilData.pupil_id
+        );
+
+      //  finalSupportAllocationData code
+      const finalSupportAllocation =
+        finalSupportAllocationData?.final_support_allocation || "";
+
+      return {
+        ...pupilData,
+        total_score: totalScoreData.total_score || 0,
+        final_support_allocation: finalSupportAllocation,
+      };
+    });
+
+    res.json(combinedData);
+  } catch (error) {
+    console.error("Error fetching pupil data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+ 
+app.listen(expressPort, () =>
+  console.log(`Listening on expressPort ${expressPort}`)
+);
