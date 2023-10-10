22c0556dd3c09326d834484d6c2c2b0924ef4d77
fowobi <109865505+fowobi@users.noreply.github.com>
2023-10-10 17:31:39 +0100

    Update the Correction on landingpage
    I created PupilRow.jsx and TeacherHeaderRow.jsx

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
-import "../styles/LandingPage.css";
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import  "../styles/LandingPage.css";
+import PupilRow from "./PupilRow";
+import TableHeaderRow from "./TeacherHeaderRow";
 
 function LandingPage() {
   const [pupils, setPupils] = useState([]);
-  
+
   useEffect(() => {
     fetch("http://localhost:5000/PupilWithTotalScoreAndSupportAllocation")
       .then((response) => {
@@ -21,73 +23,46 @@ function LandingPage() {
         console.error("Error fetching data:", error);
       });
   }, []);
-  // Function to delete a pupil record
   const deletePupil = (pupilId) => {
-    // Show a confirmation dialog
-    const confirmDelete = window.confirm("Are you sure you want to delete this pupil?");
-    
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this pupil?"
+    );
     if (confirmDelete) {
-    // Send a DELETE request to the server
-    fetch(`http://localhost:5000/pupil/${pupilId}`, {
-      method: "DELETE",
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        // Remove the deleted pupil from the local state
-        setPupils((prevPupils) =>
-          prevPupils.filter((pupil) => pupil.pupil_id !== pupilId)
-        );
+      fetch(`http://localhost:5000/pupil/${pupilId}`, {
+        method: "DELETE",
       })
-      .catch((error) => {
-        console.error("Error deleting pupil:", error);
-      });
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          setPupils((prevPupils) =>
+            prevPupils.filter((pupil) => pupil.pupil_id !== pupilId)
+          );
+        })
+        .catch((error) => {
+          console.error("Error deleting pupil:", error);
+        });
+    }
   };
-  }
 
   return (
     <div>
-      <h1>Welcome to landing page</h1>
+      <h1>Welcome to Landing page</h1>
       <Link to="/form">Add New Pupil</Link>
       {pupils.length === 0 ? (
         <p>No pupils data available.</p>
       ) : (
         <table className="pupil-table">
           <thead>
-            <tr>
-              <th>Pupil ID</th>
-              <th>Pupil Nickname</th>
-              <th>Last Update</th>
-              <th>Total Score</th>
-              <th>Override Score</th>
-              <th>Final Support Allocation</th>
-              <th>Summary</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
+            <TableHeaderRow />
           </thead>
           <tbody>
             {pupils.map((pupil) => (
-              <tr key={pupil.pupil_id}>
-                <td>{pupil.pupil_id}</td>
-                <td>{pupil.pupil_nickname}</td>
-                <td>{pupil.last_update}</td>
-                <td>{pupil.total_score}</td>
-                <td>{pupil.override_score}</td>
-                <td>{pupil.final_support_allocation}</td>
-                <td>
-                  <button>Summary</button>
-                </td>
-                <td>
-                  <button>Edit</button>
-                </td>
-               <td>
-                  <button onClick={() => deletePupil(pupil.pupil_id)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
+              <PupilRow
+                key={pupil.pupil_id}
+                pupil={pupil}
+                onDelete={deletePupil}
+              />
             ))}
           </tbody>
         </table>
@@ -97,4 +72,5 @@ function LandingPage() {
 }
 
 export default LandingPage;
+
 
