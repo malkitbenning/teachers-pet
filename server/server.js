f86b28ee8514786c01865fb241828fef323cd30e
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-10-12 15:13:15 +0100

    Moved validateUser function to module

--- server/server.js
+++ server/server.js
@@ -23,29 +23,7 @@ const client = require("./db-client");
 
 const deletePupil = require("./delete-pupil");
 const restorePupil = require("./restore-pupil");
-
-const validateUser = (req, res) => {
-  const { teacherUsername, teacherPassword } = req.body;
-
-  client.query("SELECT * FROM teacher").then((result) => {
-    console.log(result.rows);
-    const teacherID = result.rows.teacher_id;
-    res.status(200).json(result.rows);
-
-    // if (teacherPassword && teacherUsername) {
-    //   console.log(result.rows);
-    //   res.status(200).json(result.rows);
-    // }
-
-    // if (result) {
-    //   res
-    //     .status(200)
-    //     .json({ message: "Login successful.", teacherID: result });
-    // } else {
-    //   res.status(401).json({ error: "Login failed, please try again." });
-    // }
-  });
-};
+const validateUser = require("./validate-user");
 
 app.post("/login", validateUser);
 app.delete("/delete-pupil", deletePupil);
--- server/validate-user.js
+++ server/validate-user.js
@@ -1,3 +1,26 @@
 const client = require("./db-client");
 
-module.exports = deletePupil;
+const validateUser = (req, res) => {
+  const { teacherUsername, teacherPassword } = req.body;
+
+  client.query("SELECT * FROM teacher").then((result) => {
+    console.log(result.rows);
+    const teacherID = result.rows.teacher_id;
+    res.status(200).json(result.rows);
+
+    // if (teacherPassword && teacherUsername) {
+    //   console.log(result.rows);
+    //   res.status(200).json(result.rows);
+    // }
+
+    // if (result) {
+    //   res
+    //     .status(200)
+    //     .json({ message: "Login successful.", teacherID: result });
+    // } else {
+    //   res.status(401).json({ error: "Login failed, please try again." });
+    // }
+  });
+};
+
+module.exports = validateUser;
