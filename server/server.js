f1cfdae90a86227869cd1600dd0234658baddf6d
Haroun Alarabi <alarabiharoun@gmail.com>
2023-10-12 17:05:32 +0100

    added query to fill form

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const app = express();
 let cors = require("cors");
 app.use(cors());
-const { client, connectDatabase } = require("./db-client");
-connectDatabase();
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,11 +19,10 @@ const expressPort = process.env.PORT || 5000;
 
 app.listen(expressPort, () => console.log(`Listening on expressPort ${expressPort}`));
 
-const client = require("./db-client");
-
 const fetchPupilData = require("./fetchPupilData");
 const deletePupil = require("./delete-pupil");
 const restorePupil = require("./restore-pupil");
+const getQandA = require("./getQandA");
 
 const validateUser = (req, res) => {
   const { teacherUsername, teacherPassword } = req.body;
@@ -35,12 +34,9 @@ const validateUser = (req, res) => {
   }
 };
 
-app.listen(expressPort, () =>
-  console.log(`Listening on expressPort ${expressPort}`)
-);
 
+app.get("/getQandA", getQandA);
 app.get("/fetch-pupil-data", fetchPupilData);
 app.post("/login", validateUser);
 app.delete("/delete-pupil", deletePupil);
 app.post("/restore-pupil", restorePupil);
-
