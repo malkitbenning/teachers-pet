4858b076492fb92767996c8b96029bcc2c7c8207
malkitbenning <98656819+malkitbenning@users.noreply.github.com>
2023-10-17 17:59:44 +0100

    Update PrintResult.jsx with merge code

--- client/src/components/PrintResult.jsx
+++ client/src/components/PrintResult.jsx
@@ -24,46 +24,50 @@ function PrintResult({
           <p>Pupil Name: {pupilName}</p>
           <p>Date: {date}</p>
         </div>
-        <tbody>
-          {Object.keys(selectedAnswers).map((questionIndex) => {
-            const que = questions[questionIndex];
-            const answer = que.answers.find((ans) => ans.answer_id === selectedAnswers[questionIndex]);
-            const commentForAnswer = comments[questionIndex] || "";
-            return (
-              <React.Fragment key={questionIndex} className={`${questionIndex / 5 === 0 && "pageBreak"}`}>
-                <div className="question-container">
-                  <tr className="question">
-                    <td colSpan="12">
-                      <h3>
-                        Criterion{que.criterion_code}: {que.question_text}
-                      </h3>
-                    </td>
-                    <td className="score" colSpan="1">
-                      <h3 className="title">Score </h3>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td colSpan="12" className="answer-text">
-                      {answer.answer_text}
-                    </td>
-                    <td className="score-n" rowSpan="2">
-                      <span className="title-score">{answer.answer_score}</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td colSpan="12">Teacher comments: {commentForAnswer}</td>
-                  </tr>
-                </div>
-              </React.Fragment>
+         <tbody>
+            {Object.keys(selectedAnswers).map((questionIndex) => {
+              const question = questions[questionIndex];
+              const answerId = selectedAnswers[questionIndex];
+              const commentForAnswer = comments[questionIndex] || "";
+              return (
+                <React.Fragment key={questionIndex} className={`${questionIndex / 5 === 0 && "pageBreak"}`}>
+                  <div className="question-container">
+                    <tr className={`question`}>
+                      <td colSpan="12">
+                        <h3>
+                          Criterion {question.criterion_code}: {question.question_text}
+                        </h3>
+                      </td>
+                      <td className="score" colSpan="1">
+                        <h3 className="title">Score {question.answer_score}</h3>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan="12" className="answer-text">
+                        {answerId ? question.answers.find((ans) => ans.answer_id === answerId).answer_text : "N/A"}
+                      </td>
+                      <td className="score-n" rowSpan="2">
+                        <span className="title-score">
+                          {answerId ? question.answers.find((ans) => ans.answer_id === answerId).answer_score : "N/A"}
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan="12">Teacher comments: {commentForAnswer}</td>
+                    </tr>
+                   </div>  
+                </React.Fragment>
             );
           })}
         </tbody>
-        <TeacherOverride
-          totalScore={totalScore}
-          setTotalScore={setTotalScore}
-          overrideScore={overrideScore}
-          setOverrideScore={setOverrideScore}
-        />
+           <TeacherOverride
+              totalScore={totalScore}
+              setTotalScore={setTotalScore}
+              overrideScore={overrideScore}
+              setOverrideScore={setOverrideScore}
+              overrideComment={overrideComment}
+              setOverrideComment={setOverrideComment}              
+            />
       </table>
     </div>
   );
