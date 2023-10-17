8f223093f285e0c76f53a893881e3c48d6626c0b
malkitbenning <malkitbenning@gmail.com>
2023-10-17 16:47:46 +0100

    margin on print page breaks looking ok

--- client/src/components/PrintResult.jsx
+++ client/src/components/PrintResult.jsx
@@ -16,15 +16,16 @@ function PrintResult({
 }) {
   return (
     <div id="tableAndContentToPrint">
+      <div id="pageBorder"></div>
       <h3 className="result-title">Vision Impairment Support Allocation </h3>
       <table id="tableToPrint" className="table result close-after-last-question">
         <div className="resultHeader">
-        <p >Teacher Name: {teacherName}</p>
-        <p >Pupil Name: {pupilName}</p>
-        <p >Date: {date}</p>
+          <p>Teacher Name: {teacherName}</p>
+          <p>Pupil Name: {pupilName}</p>
+          <p>Date: {date}</p>
         </div>
 
-        <tbody >
+        <tbody>
           {Object.keys(selectedAnswers).map((questionIndex) => {
             const que = questions[questionIndex];
             const answer = que.answers.find((ans) => ans.answer_id === selectedAnswers[questionIndex]);
@@ -32,27 +33,27 @@ function PrintResult({
             return (
               <React.Fragment key={questionIndex}>
                 <div className="question-container">
-                <tr className="question">
-                  <td colSpan="12">
-                    <h3>
-                      Criterion{que.criterion_code}: {que.question_text}
-                    </h3>
-                  </td>
-                  <td className="score" colSpan="1">
-                    <h3 className="title">Score </h3>
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan="12" className="answer-text">
-                    {answer.answer_text}
-                  </td>
-                  <td className="score-n" rowSpan="2">
-                    <span className="title">{answer.answer_score}</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan="12">Teacher comments: {commentForAnswer}</td>
-                </tr>
+                  <tr className="question">
+                    <td colSpan="12">
+                      <h3>
+                        Criterion{que.criterion_code}: {que.question_text}
+                      </h3>
+                    </td>
+                    <td className="score" colSpan="1">
+                      <h3 className="title">Score </h3>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan="12" className="answer-text">
+                      {answer.answer_text}
+                    </td>
+                    <td className="score-n" rowSpan="2">
+                      <p className="title">{answer.answer_score}</p>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan="12">Teacher comments: {commentForAnswer}</td>
+                  </tr>
                 </div>
               </React.Fragment>
             );
