98a0b70751604acde5f933e88203de8e048b20f6
fowobi <109865505+fowobi@users.noreply.github.com>
2023-10-19 14:22:00 +0100

    Add sort to the summary landing page

--- client/src/components/TeacherHeaderRow.jsx
+++ client/src/components/TeacherHeaderRow.jsx
@@ -1,18 +1,44 @@
 import React from "react";
+import "../styles/TableHeaderRow.css";
 
+  function TableHeaderRow({ onSort, sortColumn, sortOrder }) {
+    const isSortable = (column) => {
+      return column === "pupil_name" || column === "last_update" || column === "support_code";
+    };
 
-function TableHeaderRow() {
-  return (
-    <tr>
-      <th>Pupil Name</th>
-      <th>Last Update</th>
-      <th>Total Score</th>
-      <th>Override Score</th>
-      <th>Support Code</th>
-      <th>Support Allocation</th>
-      
-    </tr>
-  );
-}
+    const getSortIndicator = (column) => {
+      if (column === sortColumn) {
+        return sortOrder === "asc" ? "▲" : "▼";
+      }
+      return "";
+    };
+    return (
+      <tr>
+        <th
+          onClick={() => isSortable("pupil_name") && onSort("pupil_name")}
+          className={`sortable-header ${isSortable("pupil_name") ? "sortable" : ""}`}
+        >
+          Pupil Name {isSortable("pupil_name") && getSortIndicator("pupil_name")}
+        </th>
+        
+        <th
+          onClick={() => isSortable("last_update") && onSort("last_update")}
+          className={`sortable-header ${isSortable("last_update") ? "sortable" : ""}`}
+        >
+          Last Update {isSortable("last_update") && getSortIndicator("last_update")}
+        </th>
+        <th>Total Score</th>
+        <th>Override Score</th>
+        
+        <th
+          onClick={() => isSortable("support_code") && onSort("support_code")}
+          className={`sortable-header ${isSortable("support_code") ? "sortable" : ""}`}
+        >
+          Support Code {isSortable("support_code") && getSortIndicator("support_code")}
+        </th>
+        <th>Support Allocation</th>
+      </tr>
+    );
+  }
 
 export default TableHeaderRow;
